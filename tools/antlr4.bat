40dadc16bf32bcb53e54d6bc8c15c6e84fc3fe3f
Adam Barnes <ambarnes@gmail.com>
2025-06-05 13:59:36 +0100

    Use pyVHDLParser grammar

--- tools/antlr4.bat
+++ tools/antlr4.bat
@@ -1,4 +1,8 @@
 @echo off
 set "TOOLS_DIR=%~dp0"
 set "VHDL_DIR=%TOOLS_DIR%..\PyHDLio\pyhdlio\vhdl\grammar"
-java -jar antlr-4.13.2-complete.jar -Dlanguage=Python3 -visitor -o "%VHDL_DIR%" "%VHDL_DIR%/vhdl.g4"
+set "VHDL_LEXER=%VHDL_DIR%\VHDLLexer.g4"
+set "VHDL_PARSER=%VHDL_DIR%\VHDLParser.g4"
+
+java -jar %TOOLS_DIR%\antlr-4.13.2-complete.jar -Dlanguage=Python3 -visitor -o "%VHDL_DIR%" "%VHDL_LEXER%"
+java -jar %TOOLS_DIR%\antlr-4.13.2-complete.jar -Dlanguage=Python3 -visitor -o "%VHDL_DIR%" "%VHDL_PARSER%"
